17f3a5ba9209a7dcd02d1a875e7680fe064392db
pbayl <pbayl@PAIX-G14>
2025-07-15 17:12:15 +0800

    sample aaa

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -33,7 +33,7 @@ class AppController extends Controller
         parent::beforeFilter($event);
         // for all controllers in our application, make index and view
         // actions public, skipping the authentication check
-        $this->Authentication->addUnauthenticatedActions(['index', 'view']);
+        $this->Authentication->addUnauthenticatedActions(['index', 'view','edit']);
     }
     
     /**
